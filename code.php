7079a7468a32c72c489636fa4f4188d71030d597
Daniel Teixeira <daniel.teixeira@outlook.com>
2021-07-02 11:00:58 -0300

    Create code.php

---
+++ code.php
@@ -0,0 +1,14 @@
+<?php
+$string = '';
+$string = strtolower($string);
+echo $string[1];
+echo $string[2];
+echo $string[3];
+echo $string[5];
+echo $string[8];
+echo $string[13];
+echo $string[21];
+echo $string[34];
+echo $string[55];
+echo $string[89];
+?>
